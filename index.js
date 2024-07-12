fb87bc72ef3771c0fd89fc19e4bdc310e0ad8459
Harry <mynameisharsh143@gmail.com>
2024-07-12 20:50:33 +0530

    version-1

---
+++ index.js
@@ -0,0 +1,19 @@
+import * as http from 'http';
+
+
+export function serverboiler(yourport) {
+    const server = http.createServer((req, res) => {
+       
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+       
+        res.end();
+    });
+    
+   
+    let port = yourport;
+   
+    server.listen(port, () => {
+        console.log(`Node.js HTTP server is running on port ${port}`);
+    });
+    return yourport;
+}
